e1238d522974d47819ee6f460f9b6929c6c8123b
Giulio Laura <gleminem1@gmail.com>
2025-11-13 12:17:41 +0100

    fixed and updated for the latest mumu version

--- Setup.bat
+++ Setup.bat
@@ -42,6 +42,16 @@ for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
         echo Found MuMu at: !vms_path!
         goto :process_vms
     )
+    if exist "%%d:\Program Files\Netease\MuMu\vms" (
+        set "vms_path=%%d:\Program Files\Netease\MuMu\vms"
+        echo Found MuMu at: !vms_path!
+        goto :process_vms
+    )
+    if exist "%%d:\Program Files (x86)\Netease\MuMu\vms" (
+        set "vms_path=%%d:\Program Files (x86)\Netease\MuMu\vms"
+        echo Found MuMu at: !vms_path!
+        goto :process_vms
+    )
 )
 
 if not defined vms_path (
@@ -93,28 +103,35 @@ for /d %%f in ("!vms_path!\*") do (
         
         rem Process vm_config.json
         set "vm_config_file=%%f\configs\vm_config.json"
+
+        rem echo Checking vm_config at: "!vm_config_file!"
+
         if exist "!vm_config_file!" (
             if not exist "!vm_config_file!.bak" (
                 copy "!vm_config_file!" "!vm_config_file!.bak" > nul
             )
-            
+
             rem Create temp file and process in one pass
             type nul > "%%f\configs\vm_config_temp.json"
+
             for /f "usebackq delims=" %%l in ("!vm_config_file!") do (
                 set "line=%%l"
                 echo !line! | find """sharable"": ""Readonly""" > nul
-                if not errorlevel 1 (
+                if !errorlevel! EQU 0 (
                     set "line=!line:"sharable": "Readonly"="sharable": "Writable"!"
                 )
                 echo !line!>> "%%f\configs\vm_config_temp.json"
             )
+
             move /y "%%f\configs\vm_config_temp.json" "!vm_config_file!" > nul
             echo [%%~nxf] Updated sharable in vm_config.json
+        ) else (
+            echo [%%~nxf] vm_config.json not found in "%%f\configs"
         )
     )
 )
 
 echo Done
-
+pause
 :end
-endlocal 
+endlocal
